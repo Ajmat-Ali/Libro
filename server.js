18247107f37ce9431a22bdab0fbcc306f22c3d0f
Ajmat-Ali <badshah9631619358@gmail.com>
2026-04-24 19:58:41 +0530

    Initial commit: Set up Libro project with Express, MongoDB, and folder structure

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const connectDB = require("./src/config/db");
+const app = require("./src/app");
+
+const PORT = process.env.PORT || 2002;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`DB connection failed: ${err.message}`);
+    process.exit(1);
+  });
